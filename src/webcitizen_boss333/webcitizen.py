812f1be7360639f1dcc08358be138e575d5949f8
ledzee <30556897+prasannakulkarni333@users.noreply.github.com>
2024-02-03 19:25:45 -0800

    0.0.2

--- src/webcitizen_boss333/webcitizen.py
+++ src/webcitizen_boss333/webcitizen.py
@@ -1,29 +1,47 @@
 # open file
-import requests
+import bs4
+import newspaper
 
-html_text_to_check = requests.get('https://www.google.com').text
-
-
-print(html_text_to_check)
-
-"""
-
-
-def traversecheck_html(html:str):
-    import bs4
+def responsive_check(html:str):
+    """
+    Check if the website is responsive
+    Args:
+        html (str): html content of the website
+    Returns:
+        int: score
+        list: message
+    """    
     x = bs4.BeautifulSoup(html, 'html.parser')
     # get meta viewport tag value
     message = []
     score = 0
-    meta_viewport_value = x.find('meta', {'name': 'viewport'})
-    print((meta_viewport_value))
-    meta_viewport_value = meta_viewport_value.get('content')
-    if str(meta_viewport_value) == 'width=device-width, initial-scale=1.0':
+    if str(x.find('meta', {'name': 'viewport'}).get('content')) == 'width=device-width, initial-scale=1.0':
         score += 10
         message.append('Meta viewport tag is set correctly')
-    # get all links
-    # for link in x.find_all('a'):
-        # print(link.get('href'))
+    # check if there are media queries in the html 
+    if '@media' in html:
+        score += 10
+        message.append('Media queries are present')
+    if "max-width" in html:
+        score += 10
+        message.append('Max-width is present')
+    
     return score, message
-traversecheck_html(html_text_to_check)
-"""
+
+def searchengine_check(html:str):
+    x = bs4.BeautifulSoup(html, 'html.parser')
+    # get meta viewport tag value
+    message = []
+    score = 0
+    # check if there are meta description tag
+    if x.find('meta', {'name': 'description'}):
+        score += 10
+        message.append('Meta description tag is present')
+    # check if page has H1 tag
+    if x.find('h1'):
+        score += 10
+        message.append('H1 tag is present')
+    
+    return score, message
+
+print(searchengine_check(html_text_to_check))
